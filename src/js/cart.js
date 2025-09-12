c5344f871b7534e569361488a7db589aee27aa70
anwelli <azukah3@gmail.com>
2025-09-12 08:48:55 -0700

    add

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,8 +1,8 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-const htmlItems = cartItems && Array.isArray(cartItems) ? cartItems.map((item) => cartItemTemplate(item)) : [];
+  const cartItems = getLocalStorage("cart");
+  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
